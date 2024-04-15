7363809ac9ac1f3578962640502bbfb5f2d3b042
Katheryner <149072343+Katheryner@users.noreply.github.com>
2024-04-15 08:29:51 -0500

    Add files via upload

--- hogwarts/js/app.js
+++ hogwarts/js/app.js
@@ -1,294 +1,93 @@
-let persona = {
+const nombre = document.querySelector("#name");
+const edad = document.querySelector("#edad");
+const familia = document.querySelector("#familia");
+const linaje = document.querySelector("#linaje");
+const cualidades = document.querySelector("#cualidades");
+
+const boton = document.querySelector("#registrar");
+const boton2 = document.querySelector("#casa");
+
+const valores = document.querySelector(".seleValores");
+const inicio = document.querySelector(".estudiante");
+const bienvenida = document.querySelector(".bienvenida");
+
+const bienvenidaTitulo = bienvenida.firstChild.nextSibling;
+const cargando = document.querySelector(".cargando");
+
+const musica = document.querySelector("#harry");
+musica.play();
+
+const aplausos = document.querySelector("#aplausos");
+
+
+
+let person = {
   nombre: "",
   edad: "",
   familia: "",
   linaje: "",
   casa: "",
   animalp: "",
-  cualidades: "valor,fuerza,audacia",
+  cualidades: "",
 };
 
-persona.nombre = prompt("Ingresa tu nombre");
-persona.edad = prompt("Ingresa tu edad");
-persona["familia"] = prompt("Ingresa tu familia (Apellido)");
-("");
+boton.addEventListener("click", (e) => {
+  e.preventDefault();
+  person.nombre = nombre.value;
+  person.edad = edad.value;
+  person.familia = familia.value;
+  person.linaje = linaje.value;
 
-const opcion = prompt(`
-Ingrese su linaje: 
-1.muggle
-2.mestizo
-3.sangre pura
-`);
+  console.log(linaje.value);
 
-if (opcion == "1") {
-  persona.linaje = "muggle";
-} else if (opcion == "2") {
-  persona.linaje = "mestizo";
-} else if (opcion == "3") {
-  persona.linaje = "sangre pura";
-} else {
-  console.log("opcion no vàlida");
-}
+  inicio.style.display = "none";
+  valores.style.display = "flex";
+});
 
-function mostrarDatos() {
-  let mensaje = "";
-  for (const key in persona) {
-    console.log(persona[key]);
-    mensaje += `
-    ${key}: ${persona[key]}`;
-  }
+boton2.addEventListener("click", (e) => {
+  e.preventDefault();
+  
+  setTimeout(() => {
+    cargando.style.display = "none";
+    bienvenidaTitulo.style.display = "flex";
+    aplausos.play();
+  }, 6000);
 
-  alert(mensaje);
-}
+  person.cualidades = cualidades.value;
+  console.log(person.cualidades);
+  seleccionarCasa();
+
+  valores.style.display = "none";
+  bienvenida.style.display = "flex";
+  console.log(person.casa);
+
+});
 
 function seleccionarCasa() {
   if (
-    (persona.cualidades === "valor,fuerza,audacia" &&
-      persona.linaje === "muggle") ||
-    persona.linaje === "mestizo" ||
-    persona.linaje === "sangre pura"
+    person.cualidades === "Valor,Fuerza,Audacia" &&
+    (person.linaje === "muggle" ||
+      person.linaje === "mestizo" ||
+      person.linaje === "sangre pura")
   ) {
-    console.log("Perteneces a Gryffindor");
-    persona.casa = "Gryffindor";
+    person.casa = "Gryffindor";
   } else if (
-    (persona.cualidades === "Justicia, Lealtad, Paciencia" &&
-      persona.linaje === "muggle") ||
-    persona.linaje === "mestizo"
+    person.cualidades === "Justicia, Lealtad, Paciencia" &&
+    (person.linaje === "muggle" || person.linaje === "mestizo")
   ) {
-    console.log("Perteneces a Hufflepuff");
-    persona.casa = "Hufflepuff";
+    person.casa = "Hufflepuff";
   } else if (
-    (persona.cualidades === "Creatividad, Erudición, Inteligencia" &&
-      persona.linaje === "muggle") ||
-    persona.linaje === "mestizo"
+    person.cualidades === "Creatividad, Erudición, Inteligencia" &&
+    (person.linaje === "muggle" || person.linaje === "mestizo")
   ) {
-    console.log("Perteneces a Ravenclaw");
-    persona.casa = "Ravenclaw";
+    person.casa = "Ravenclaw";
   } else if (
-    persona.cualidades === "Ambición, Determinación, Astucia" &&
-    persona.linaje === "sangre pura"
+    person.cualidades === "Ambición, Determinación, Astucia" &&
+    person.linaje === "sangre pura"
   ) {
-    console.log("Perteneces a Slytherin");
-    persona.casa = "Slytherin";
-  }
-}
-seleccionarCasa();
-mostrarDatos();
-const profesores = {
-  transformaciones: "Kevin Slughorn",
-  herbologia: "Maria Umbridge",
-  pociones: "Liliana McGonagall",
-  encantamientos: "Jackie",
-  defensaContraLasArtesOscuras: "Robinson Snape",
-  animalesMagicos: "David Flich",
-  historiaDeMagia: "Ronald Sprout",
-  mostrarProfesores: function () {
-    let mensaje = "lista de profesores:";
-    for (const key in profesores) {
-      if (key == "mostrarProfesores" || key == "cambiarProfesor") {
-        break;
-      } else {
-        mensaje += `
-                ${key}, profesor: ${profesores[key]}
-                `;
-      }
-    }
-    mostrarMensaje(mensaje);
-  },
-  cambiarProfesor: function (profesor1) {
-    mostrarMensaje(`Estas cambiando el profesor ${profesor1}`);
-    const cambio = profesores.animalesMagicos;
-    mostrarMensaje(
-      `Has seleccionado cambiar el profesor ${profesor1} por ${cambio}`
-    );
-    for (const key in profesores) {
-      if (profesores[key] === cambio) {
-        profesores[key] = profesor1;
-        console.log(profesores[key]);
-      }
-    }
-    for (const key in profesores) {
-      if (profesores[key] === profesor1) {
-        profesores[key] = cambio;
-        console.log(profesores[key]);
-      }
-    }
-    mensajeExitoso("Se cambiaron los profesores exitosamente!!");
-    this.mostrarProfesores();
-  },
-};
-
-function mostrarMensaje(mensaje = "Digite su mensaje") {
-  alert(mensaje);
-}
-
-function mensajeExitoso(mensaje = "Exitoso!!") {
-  alert(mensaje);
-}
-
-function claseTransformaciones() {
-  mostrarMensaje("Entraste a clase de transformaciones");
-  const transformaciones = {
-    nombreProfesor: "Kevin Slughorn",
-    horario: "15:00",
-    rastrearTransformacion: false,
-    hechizoRiddikulus: function () {
-      if (this.rastrearTransformacion) {
-        mostrarMensaje(`Realizando el hechizo Riddikulus...`);
-      } else {
-        mostrarMensaje("No hay boggart presente");
-      }
-    },
-    enfrentarBoggart: function (boggart) {
-      mostrarMensaje(`Enfrentando al boggart
-      El estado del boggart es: ${boggart}`);
-      mostrarMensaje(`Realizamos el hechizo riddikulus`);
-      this.hechizoRiddikulus();
-      boggart = "boggart transformado";
-      mostrarMensaje(`el resultado de la transformacion es ${boggart}`);
-      mostrarMensaje("Has vencido al boggart");
-    },
-  };
-  let boggartEjemplo = "boggart original";
-  mostrarMensaje("Un boggart ha aparecido");
-  transformaciones.rastrearTransformacion = true;
-  transformaciones.enfrentarBoggart(boggartEjemplo);
-  mostrarMensaje("Termino la clase de transformaciones");
-}
-
-claseTransformaciones();
-
-function claseDefensaArtesOscuras() {
-  mostrarMensaje("Entraste a clase de artes oscuras");
-  const defensaContraLasArtesOscuras = {
-    nombreProfesor: "Robinson Snape",
-    horario: "17:00",
-    generarAnimalPatronus: function () {
-      mostrarMensaje("Seleccionando animal de patronus...");
-      const animal = Math.floor(Math.random() * 8);
-
-      switch (animal) {
-        case 0:
-          persona.animalPatronus = "Murcielago";
-          mensajeExitoso();
-          break;
-        case 1:
-          persona.animalPatronus = "Toro";
-          mensajeExitoso();
-          break;
-        case 2:
-          persona.animalPatronus = "Gato";
-          mensajeExitoso();
-          break;
-        case 3:
-          persona.animalPatronus = "Cabra";
-          mensajeExitoso();
-          break;
-        case 4:
-          persona.animalPatronus = "Caballo";
-          mensajeExitoso();
-          break;
-        case 5:
-          persona.animalPatronus = "Conejo";
-          mensajeExitoso();
-          break;
-        case 6:
-          persona.animalPatronus = "Escorpion";
-          mensajeExitoso();
-          break;
-        case 7:
-          persona.animalPatronus = "Serpiente";
-          mensajeExitoso();
-          break;
-        default:
-          mostrarMensaje("Opcion no valida");
-          break;
-      }
-    },
-  };
-  defensaContraLasArtesOscuras.generarAnimalPatronus();
-  mostrarMensaje("Termino la clase de defensa de artes oscuras");
-}
-
-claseDefensaArtesOscuras();
-
-function dementores() {
-  mostrarMensaje(
-    "En tu camino te has encontrado un dementor y solo puedes defenderte si tienes un animal patronus"
-  );
-  alert(persona.animalPatronus);
-  if (persona.animalPatronus.length > 0) {
-    mostrarMensaje("Te has defendido con tu animal patronus");
+    person.casa = "Slytherin";
   } else {
-    mostrarMensaje(
-      "No tienes animal patronus no te puedes defender contra los dementores"
-    );
+    person.casa = "Gryffindor";
   }
+  bienvenidaTitulo.innerHTML = ` ${person.nombre}, ¡Bienvenido a ${person.casa}!`;
 }
-dementores();
-
-function clasePociones() {
-  mostrarMensaje("Entraste a clase de pociones");
-  const pociones = {
-    nombreProfesor: "Liliana McGonagall",
-    horario: "19:00",
-    crisopos: 2,
-    talloDeDescurainiaSophia: 1,
-    tiempoPreparacion: 5, //minutos
-    pocionFelixFelicis: 0,
-    verMateriales: function () {
-      mostrarMensaje(`Materiales:
-      Crisopos = ${this.crisopos}
-      Tallo de descurainia sophia ${this.talloDeDescurainiaSophia}`);
-    },
-    prepararPocion: function (
-      crisopos,
-      talloDeDescurainiaSophia,
-      tiempoPreparacion
-    ) {
-      if (crisopos >= 2 && talloDeDescurainiaSophia >= 1) {
-        if (tiempoPreparacion == this.tiempoPreparacion) {
-          mostrarMensaje(`Has preparado correctamente "Pocion Felix Felicis"`);
-          this.pocionFelixFelicis += 1;
-        } else {
-          mostrarMensaje(`El tiempo de preparacion debe ser de 5 minutos`);
-          mostrarMensaje(`la pocion ha fallado y te has hecho danio`);
-        }
-      } else {
-        mostrarMensaje(`No tienes los suficientes ingredientes para realizar la pocion
-        Materiales necesarios:
-        Crisopos x2
-        Tallo de descurainia sophia x1
-        Materiales en inventario:
-        Crisopos = ${crisopos}
-        Tallo de descurainia sophia = ${talloDeDescurainiaSophia}`);
-        mostrarMensaje(`la pocion ha fallado y te has hecho danio`);
-      }
-      this.crisopos -= crisopos;
-      this.talloDeDescurainiaSophia -= talloDeDescurainiaSophia;
-    },
-    usarPocionFelixFelicis: function () {
-      if (this.pocionFelixFelicis >= 1) {
-        const profesor1 = profesores.pociones;
-        profesores.cambiarProfesor(profesor1);
-        this.pocionFelixFelicis -= 1;
-        mostrarMensaje("Cambiaste el profesor con una pocion de Felix Felicis");
-      } else {
-        mostrarMensaje(`No tienes suficientes pociones.
-        Inventario de pociones:
-        Pocion Felix Felicis = ${this.pocionFelixFelicis}`);
-      }
-    },
-  };
-  pociones.verMateriales();
-  pociones.prepararPocion(
-    pociones.crisopos,
-    pociones.talloDeDescurainiaSophia,
-    pociones.tiempoPreparacion
-  );
-  pociones.usarPocionFelixFelicis();
-  mostrarMensaje("Has terminado la clase de pociones");
-}
-
-clasePociones();
-mostrarDatos();
